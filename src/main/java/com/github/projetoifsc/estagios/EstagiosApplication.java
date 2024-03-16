a7fa4014226f1f82c0ed3a3bbed846a3b4ce5705
juhachmann <juliana.hachmann@gmail.com>
2024-03-16 18:38:47 -0300

    Projeto Iniciado. Dependências atualizadas

---
+++ src/main/java/com/github/projetoifsc/estagios/EstagiosApplication.java
@@ -0,0 +1,13 @@
+package com.github.projetoifsc.estagios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagiosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstagiosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/projetoifsc/estagios/EstagiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projetoifsc.estagios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagiosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
