19b3aa0a8071d3abf192b0dc96db360627cb92e2
juhachmann <juliana.hachmann@gmail.com>
2024-04-07 15:16:17 -0300

    API 00 - Removed Datasource from temporary requirements for API

--- src/main/java/com/github/projetoifsc/estagios/EstagiosApplication.java
+++ src/main/java/com/github/projetoifsc/estagios/EstagiosApplication.java
@@ -1,9 +1,12 @@
 package com.github.projetoifsc.estagios;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EstagiosApplication {
 
     public static void main(String[] args) {
