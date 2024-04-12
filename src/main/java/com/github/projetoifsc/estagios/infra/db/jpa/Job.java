7042cd02f91e1e85e85896bcc617b6c05216ff50
juhachmann <juliana.hachmann@gmail.com>
2024-04-12 12:10:15 -0300

    DB 02 - Esquema de Jobs alterado para aceitar apenas o valor do id ao inserir período,formato e nível (ver: https://stackoverflow.com/questions/27930449/jpa-many-to-one-relation-need-to-save-only-id

--- src/main/java/com/github/projetoifsc/estagios/infra/db/jpa/Job.java
+++ src/main/java/com/github/projetoifsc/estagios/infra/db/jpa/Job.java
@@ -1,10 +1,18 @@
 package com.github.projetoifsc.estagios.infra.db.jpa;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+
+// Ver a questão do ID
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "jobs")
 class Job {
 
@@ -14,49 +22,65 @@ class Job {
 
     // TODO Ver Projection (Proxy interface ou Record)
     // https://docs.spring.io/spring-data/jpa/reference/repositories/projections.html
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id")
     Organization owner;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name="areas_jobs",
         joinColumns = @JoinColumn(name="job_id"),
         inverseJoinColumns = @JoinColumn(name="area_id")
     )
     List<Area> areas;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "ies_received_jobs",
         joinColumns = @JoinColumn(name = "job_id"),
         inverseJoinColumns = @JoinColumn(name = "org_id")
     )
     List<Organization> ies;
 
+    // Como pode vir das outras camadas:
+    // Com um id OU com os campos marcados.. (a camada de negócio vai ter que resolver isso)
     // Ver aqui, pq vai pegar o id de uma tabela que tá relacionada com organização
     // Ver bem como vai fazer aqui!
     // A organização precisa cadastrar o endereço e aí depois cadastrar a vaga, ou só depois associar os dois
     // Ver como faz transaction corretamente com o Spring Data ! (maneja automaticamente?)
     // Como testar falhas em transactions? (Boa pergunta pra jogar para o Bruno!)
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "address_id")
     Address address;
 
     // Mesma coisa do caso acima
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "contact_id")
     Contact contact;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "period_id")
-    Period periodos;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "study_level_id")
+    // Solução: https://stackoverflow.com/questions/27930449/jpa-many-to-one-relation-need-to-save-only-id
+    // Funciona, pois acusa falha de restrição de FK quando insere um valor que não existe na tabela relacionada
+    // Não funciona caso, na mesma transação, deseje salvar E selecionar a tabela relacionada
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "period_id", insertable = false, updatable = false)
+    Period periodo;
+
+    @Column(name = "period_id")
+    short periodId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_level_id", insertable = false, updatable = false)
     Level nivel;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "format_id")
+    @Column(name = "study_level_id")
+    short levelId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "format_id", insertable = false,updatable = false)
     Format format;
+
+    @Column(name = "format_id")
+    short formatId;
 
 
     String titulo;
@@ -80,8 +104,14 @@ class Job {
 
     int carga_horaria_semanal;
     // TODO Criação e Atualização automáticos
-    String criado_em;
-    String atualizado_em;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    LocalDateTime updatedAt;
 
 
 
@@ -92,6 +122,38 @@ class Job {
     public Job() {
     }
 
+
+    Organization getOwner() {
+        return owner;
+    }
+
+    List<Area> getAreas() {
+        return areas;
+    }
+
+    List<Organization> getIes() {
+        return ies;
+    }
+
+    Address getAddress() {
+        return address;
+    }
+
+    Contact getContact() {
+        return contact;
+    }
+
+    Period getPeriodo() {
+        return periodo;
+    }
+
+    Level getNivel() {
+        return nivel;
+    }
+
+    Format getFormat() {
+        return format;
+    }
 
     @Override
     public String toString() {
@@ -106,8 +168,9 @@ class Job {
                 ", duracao_meses=" + duracao_meses +
                 ", remuneracao=" + remuneracao +
                 ", carga_horaria_semanal=" + carga_horaria_semanal +
-                ", criado_em='" + criado_em + '\'' +
-                ", atualizado_em='" + atualizado_em + '\'' +
+                ", criado_em='" + createdAt + '\'' +
+                ", atualizado_em='" + updatedAt + '\'' +
                 '}';
     }
+
 }
