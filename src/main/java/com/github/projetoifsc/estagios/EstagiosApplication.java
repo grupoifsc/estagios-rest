bcb98be814c45131738575c2ca9cbe634e59ac4d
juhachmann <juliana.hachmann@gmail.com>
2024-04-12 17:04:17 -0300

    BD 01 - Mudei local de tasks.md

--- src/main/java/com/github/projetoifsc/estagios/EstagiosApplication.java
+++ src/main/java/com/github/projetoifsc/estagios/EstagiosApplication.java
@@ -1,11 +1,8 @@
 package com.github.projetoifsc.estagios;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 @EnableJpaAuditing
